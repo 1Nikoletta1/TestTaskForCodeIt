d344979ed59dd0365e9ab5c68e6420003f3c482d
1Nikoletta1 <sayvero@gmail.com>
2016-11-11 23:24:53 +0200

    Created carousel for News Section.

--- js/parseNews.js
+++ js/parseNews.js
@@ -23,7 +23,7 @@ function showNews(responseText) {
     console.log(responseText);
 
     //получить список новостей
-    var news = document.querySelector('#news article');
+    var news = document.querySelector('#news #scroll-images');
 
     var article = [];
     var date = new Date();
@@ -51,4 +51,48 @@ function showNews(responseText) {
 
     }
 
+}
+
+
+/*-----------------------
+ ------carousel-area------
+ ----------------------*/
+
+var position = 0; // текущий сдвиг влево
+var carousel = document.getElementById('carousel');
+
+carousel.onclick = function(event) {
+
+    var target = event.target;
+
+    while (target != this) {
+        if (target.nodeName == 'BUTTON') {
+            scrollCarousel(target);
+            return false;
+        }
+        target = target.parentNode;
+    }
+};
+
+function scrollCarousel(target) {
+
+    var width = 400; // ширина изображения
+    var count = 1; // количество изображений
+
+
+    var list = carousel.querySelector('#scroll-images');
+    var listElems = carousel.querySelectorAll('.oneNew');
+
+    if ( target.classList.contains('prev') ) {
+
+        position = Math.min(position + width * count, 0);
+        list.style.marginLeft = position + 'px';
+
+
+    } else if ( target.classList.contains('next') ) {
+
+        position = Math.max(position - width * count, -width * (listElems.length - count));
+        list.style.marginLeft = position + 'px';
+
+    }
 }
